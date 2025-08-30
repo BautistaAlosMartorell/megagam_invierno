90185add430c32efb1883511707e454c497b7b66
Federico Mora <fedenahuel566@gmail.com>
2025-08-30 20:55:13 -0300

    GeneradorOxigeno

--- PlayerTest/Oxygen/oxygen_bar.gd
+++ PlayerTest/Oxygen/oxygen_bar.gd
@@ -5,7 +5,7 @@ var target_Value := 100.0
 func _process(delta):
 	if target_Value >= 0.0:
 		var aux
-		aux = move_toward(self.value, target_Value, delta * 2.0)
+		aux = move_toward(self.value, target_Value, delta * 1.0)
 		self.value = aux
 
 func Update_Bar(maxim: float, act: float):
