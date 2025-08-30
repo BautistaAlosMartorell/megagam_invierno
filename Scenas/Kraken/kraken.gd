706a0a2b6c440dbb699415758d918a79f71e4546
valenfurlani <valentinofurlani12@gmail.com>
2025-08-30 15:38:39 -0300

    kraken v1.5

--- Scenas/Kraken/kraken.gd
+++ Scenas/Kraken/kraken.gd
@@ -1,20 +1,8 @@
 extends CharacterBody2D
 
-@export var speed: float = 200.0  # velocidad de avance (igual que la del juego)
-@export var player_path: NodePath
-
-var player
-
-func _ready():
-	player = get_node(player_path)
-	$Area2D.body_entered.connect(_on_body_entered)
-
+@export var speed:float = 100
+var direction = Vector2.RIGHT
 func _physics_process(delta):
-	# Avanza hacia la derecha
-	position.x += speed * delta
-
-func _on_body_entered(body):
-	if body.name == "Player":
-		print("Jugador atrapado por el Kraken")
-		# Llamar a la lógica de Game Over, por ejemplo:
-		get_tree().change_scene_to_file("res://GameOver.tscn")
+	velocity = direction.normalized() * speed
+	move_and_slide()
+	
